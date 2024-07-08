f59aff27feaab80a5c30b0429f59469bb33ed8d1
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-07-08 10:28:05 -0400

    added weapon list formatting and functionallity

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { WeaponsComponent } from '../equipment/weapons/weapons.component';
+import { ArmorComponent } from '../equipment/armor/armor.component';
+import { AshesOfWarComponent } from '../equipment/ashes-of-war/ashes-of-war.component';
+import { ClassesComponent } from '../equipment/classes/classes.component';
+import { IncantationsComponent } from '../equipment/incantations/incantations.component';
+import { ShieldsComponent } from '../equipment/shields/shields.component';
+import { SorceriesComponent } from '../equipment/sorceries/sorceries.component';
+import { TalismansComponent } from '../equipment/talismans/talismans.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [WeaponsComponent,ArmorComponent,AshesOfWarComponent,ClassesComponent,IncantationsComponent,ShieldsComponent,SorceriesComponent,TalismansComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  toggleList:number = 0;
+
+  showList(num:number){
+    this.toggleList = num;
+  }
 
 }
--- src/app/components/equipment/weapons/weapons.component.ts
+++ src/app/components/equipment/weapons/weapons.component.ts
@@ -1,27 +1,43 @@
 import { Component } from '@angular/core';
 import { EldenRingService } from '../../../services/elden-ring.service';
 import { WeaponsModel } from '../../../models/weapons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-weapons',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './weapons.component.html',
   styleUrl: './weapons.component.css'
 })
 export class WeaponsComponent {
   constructor(private eldenringService:EldenRingService){};
 
-  currentWeapon:WeaponsModel = {} as WeaponsModel;
+  currentWeapons:WeaponsModel = {} as WeaponsModel;
+  formWeapon:string = "";
+  pageCount:number = 0;
 
   ngOnInit(){
-    this.getWeaponByName();
+    this.getWeaponList();
   }
 
   getWeaponByName(){
-    this.eldenringService.getWeaponByName("Forked Hatchet").subscribe((response:WeaponsModel) => {
-      this.currentWeapon = response;
+    this.eldenringService.getWeaponByName(this.formWeapon).subscribe((response:WeaponsModel) => {
+      this.currentWeapons = response;
       console.log(response);
     })
+  }
+
+  getWeaponList(){
+    this.eldenringService.getWeaponList(this.pageCount).subscribe((response:WeaponsModel)=>{
+      this.currentWeapons = response;
+      console.log(response);
+    })
+  }
+
+  changePage(num:number){
+    this.pageCount += num;
+    this.getWeaponList();
+    window.scroll(0,0);
   }
 }
