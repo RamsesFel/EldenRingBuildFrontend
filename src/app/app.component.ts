aefd27683d995a96aedffe4e9fab368af16d6c55
jackyweave <71942159+jackyweave@users.noreply.github.com>
2024-07-16 13:00:29 -0400

    CSS changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,38 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { WeaponsComponent } from './components/equipment/weapons/weapons.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { CreateComponent } from './components/create/create.component';
+import { GoogleSigninButtonDirective, GoogleSigninButtonModule, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WeaponsComponent, ProfileComponent, RouterLink, CreateComponent],
+  imports: [
+    RouterOutlet,
+    WeaponsComponent,
+    ProfileComponent,
+    RouterLink,
+    CreateComponent,
+    GoogleSigninButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'FrontEnd';
-
-
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+  constructor(private socialAuthServiceConfig: SocialAuthService) {}
+  ngOnInit() {
+    //authState is a custom observable that will run again any time changes are noticed.
+    this.socialAuthServiceConfig.authState.subscribe(
+      (userResponse: SocialUser) => {
+        this.user = userResponse;
+        //if login fails, it will return null.
+        this.loggedIn = userResponse != null;
+      }
+    );
+  }
+  signOut(): void {
+    this.socialAuthServiceConfig.signOut();
+  }
 }
